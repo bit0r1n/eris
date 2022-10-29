c5e3119f401559c138535f5928dd448da4a0a1dd
bit0r1n <me@bitor.in>
2022-10-29 22:56:28 +0300

    feat: resume url

--- index.d.ts
+++ index.d.ts
@@ -3364,6 +3364,7 @@ declare namespace Eris {
     ready: boolean;
     reconnectInterval: number;
     requestMembersPromise: { [s: string]: RequestMembersPromise };
+    resumeURL: string | null;
     seq: number;
     sessionID: string | null;
     status: "connecting" | "disconnected" | "handshaking" | "identifying" | "ready" | "resuming";
